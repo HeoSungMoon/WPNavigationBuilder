d1bda34e21f8fe9b3d800a2e89cbc7ae34b7b3f3
mithtia <cjdlfwm@naver.com>
2025-04-26 23:53:16 +0900

    Init Project

---
+++ Source/WPNavigationBuilder/WPNavigationBuilder.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WPNavigationBuilder.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WPNavigationBuilder, "WPNavigationBuilder" );
+
+DEFINE_LOG_CATEGORY(LogWPNavigationBuilder)
+ 
